8b711d50e70f32ed79d307bde8ab8ea6ea64894c
CPE Etudiant <simon.ernst@live.com>
2020-02-27 11:08:00 +0100

    Adding CameraInfo from sensor_msgs

--- scripts/store_data.py
+++ scripts/store_data.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from std_msgs.msg import Bool
 from geometry_msgs.msg import PolygonStamped
 import cv2
